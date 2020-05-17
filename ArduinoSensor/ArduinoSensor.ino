bb717101aa096d02b3a749a935625344ac7bc714
Sam Pratt <sampratt@SamPratt-MacBook-Pro.local>
2020-05-17 03:53:22 +0100

    Got HTTP post working on test URL.

--- ArduinoSensor/ArduinoSensor.ino
+++ ArduinoSensor/ArduinoSensor.ino
@@ -3,28 +3,44 @@
 #include <Wire.h>
 #include "BlueDot_BME280.h"
 #include <LiquidCrystal_I2C.h>
+#include "SoftwareSerial.h"
 
+#define WIFI_RX_PIN 6
+#define WIFI_TX_PIN 7
+
+#define SERIAL_SPEED 9600
+
+String host = "postman-echo.com";
+String url = "post";
+
+String ssid = "****";
+String password =  "****";
 
 BlueDot_BME280 bme; //BME280 Sensor
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+SoftwareSerial esp(WIFI_RX_PIN, WIFI_TX_PIN);
 
 int displayOnFor = 10000;
 int displayTimer = 0;
 int displayToShow = 0;
 
-int sleepFor = 10;
-int sleepUpdateIterations = 500;
+int sleepFor = 10; // Sleep 10ms between loops
+int sleepUpdateIterations = 500; //Update the sensors every 500 iterations
 
 float temp;
 float humidity;
 float pressure;
 
-int buttonPin = 7;
+int buttonPin = 8;
 
 void setup() {
   
-  Serial.begin(9600);
+  Serial.begin(SERIAL_SPEED);
   Serial.println("Starting...");
+
+  esp.begin(SERIAL_SPEED);
+  esp.setTimeout(10000);
 
   lcd.init(); 
   lcd.init();
@@ -83,6 +99,7 @@ void checkButton() {
   if(digitalRead(buttonPin) == LOW)
   {
     Serial.println("Button pushed");
+    Serial.println("Updating Temps");
     displayToShow++;
     displayTimer = displayOnFor;
     int ds = displayToShow % 3;
@@ -99,6 +116,90 @@ void checkButton() {
     }
         
   }
+}
+
+void join() {
+
+  Serial.println("Reseting WiFi");
+  esp.println("AT+RST");
+  if(esp.find("ready")) {
+    Serial.println("Module Reset OK");
+  }
+  //Let the WiFi come up if it's configured
+  Serial.print("Waiting for WiFi to start-up");
+  for(int i=0; i<20; i++) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+
+
+  Serial.println("Setting Station Mode");
+  esp.println("AT+CWMODE=1");
+  if(esp.find("OK")) {
+    Serial.println("Station Mode Set to 1");
+  }
+  
+  Serial.println("Joining WiFi");
+  String join = "AT+CWJAP=\"" + ssid + "\",\"" + password + "\"";
+  esp.println(join);
+  if(esp.find("WIFI GOT IP")) {
+    Serial.println("Joined OK");
+  }
+}
+
+void sendData() {
+  esp.println("Seting connection multiplex off");
+  esp.println("AT+CIPMUX=0");
+  if(esp.find("OK")) {
+    Serial.println("Mux Set to 0");
+  }
+
+  Serial.println("Connecting to host " + host);
+  esp.println("AT+CIPSTART=\"TCP\",\""+ host + "\",80");
+  if(esp.find("OK")) {
+    Serial.println("Connected to " + host);
+  }
+
+  String body = "POST /POST?temperature="+ (String)temp +"&pressure=" + (String)pressure + "&humidity=" + humidity + " HTTP/1.1\r\nHost: " + host;
+  Serial.println(body);
+  int bodyLength = body.length() + 4;
+  
+  esp.println("AT+CIPSEND=" + (String)bodyLength);
+  if(esp.find("OK")) {
+    Serial.println("OK to send");
+  }
+  delay(1000);
+  esp.println(body);
+  esp.println("");
+  if(esp.find("SEND OK")) {
+    Serial.println("Sent request OK");
+  }
+  esp.find("+IPD,");
+
+  int c = 0;
+  Serial.print("Got '");
+  while(true) {
+    c = esp.read();
+    if((char)c == ':') {
+      break;
+    }
+    if(c != -1) {
+      Serial.print((char)c);
+    }
+  }
+  Serial.println("' bytes");
+
+  for(int i=0; i<20000; i++) {
+    c = esp.read();
+    if(c != -1) {
+      Serial.print((char)c);
+    }
+  }
+  Serial.println("");
+  
+  Serial.println("Closing connection");
+  esp.println("AT+CIPCLOSE");
 }
 
 void updateDisplay(String label, String value, String unit, bool backlight) {
@@ -138,7 +239,9 @@ void loop() {
   if(loopCount % 500 == 0)
   {
     updateTemps();
-    upDateDisplay(); 
+    upDateDisplay();
+    join();
+    sendData(); 
   }
 
 

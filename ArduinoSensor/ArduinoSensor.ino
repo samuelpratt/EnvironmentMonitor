bdfb5d77875de74c5310293f02dc3517bad56ee5
Sam Pratt <samuel.pratt@gmail.com>
2020-05-18 14:16:27 +0100

    Added comment regarding B280 I2C address.

--- ArduinoSensor/ArduinoSensor.ino
+++ ArduinoSensor/ArduinoSensor.ino
@@ -10,7 +10,9 @@
 
 #define SERIAL_SPEED 9600
 
-// Your board may be on 0x77 mine appears to be a knock off sensor. If in doubt use an I2C Scanner
+// Your board may be on 0x77 it's changed using a soldered link on the
+// board (https://github.com/adafruit/Adafruit_BME280_Library/issues/15#issuecomment-296583253).
+// If in doubt use an I2C Scanner
 // e.g. https://github.com/RobTillaart/Arduino/tree/master/sketches/MultiSpeedI2CScanner
 #define BME280_ID  0x76
 #define LCD_ID  0x27
